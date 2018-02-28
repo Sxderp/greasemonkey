ae8ab0168481d3e14f28c80c4c63163a5fb65f15
Steven Daniele <sdaniele@gatech.edu>
2018-02-28 14:16:44 -0500

    Time value-store tests

--- test/setup.js
+++ test/setup.js
@@ -3,9 +3,27 @@ chrome.runtime.getManifest.returns({'version': 1});
 
 // See comment for details
 // https://github.com/greasemonkey/greasemonkey/pull/2812#issuecomment-358776737
+navigator.storage = {};
 navigator.storage.persist = () => Promise.resolve(true);
 
 // In tests, never complain about missing translations.
 function _(str) {
   return str;
 }
+
+(function() {
+
+const timers = {};
+
+console.time = function(label) {
+  timers[label] = performance.now();
+  console.log(label + ': timer started');
+}
+
+console.timeEnd = function(label) {
+  let f = (performance.now() - timers[label]).toString();
+  let [d, l] = f.split('.');
+  console.log(label + ': ' + d + '.' + l.substr(0,2) + 'ms');
+}
+
+})();
